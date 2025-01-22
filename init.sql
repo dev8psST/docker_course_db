7393447bb0b1472a1f6bb54a0c4098f4205e5be2
dev8psST <88630556+dev8psST@users.noreply.github.com>
2025-01-22 15:50:26 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS new_table;
 CREATE TABLE new_table (id serial PRIMARY KEY,name text NOT NULL,city varchar (50) NOT NULL,salary text NOT NULL);
-INSERT INTO new_table (name, city, salary) VALUES ('Kolya Lochok','Kolyma','689');
-INSERT INTO new_table (name, city, salary) VALUES ('Vasya Pidarok','Magadan','989');
+INSERT INTO new_table (name, city, salary) VALUES ('Kolya User','London','1689');
+INSERT INTO new_table (name, city, salary) VALUES ('Vasya Pupkin','Paris','1989');
